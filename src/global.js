e2d8e70cbba524518e70a91eeb5faee5e568c8de
krishnaveni44 <krishnaveni44@gmail.com>
2022-04-08 08:51:00 +0530

    1

--- src/global.js
+++ src/global.js
@@ -1,2 +1,3 @@
-// export const api = "http://localhost:5000";
-export const api = "https://student-mentor-portal.herokuapp.com";
+// export const api = "http://localhost:4000";
+// export const api = "https://student-mentor-portal.herokuapp.com";
+export const api = "https://studentmentortaskapp.herokuapp.com";
